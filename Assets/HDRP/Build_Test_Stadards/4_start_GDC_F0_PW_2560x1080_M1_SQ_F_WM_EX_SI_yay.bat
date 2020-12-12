b41bda2193be8c90ed2624f0a26349a6f1408118
sasa42 <keeponshading@gmail.com>
2020-12-12 20:35:52 +0100

    2019.2.0b14 HDRP 10.2.0 TAA High Reprojection setup

---
+++ Assets/HDRP/Build_Test_Stadards/4_start_GDC_F0_PW_2560x1080_M1_SQ_F_WM_EX_SI_yay.bat
@@ -0,0 +1,5 @@
+
+
+start /D .\424 VPP_Perrinn_424_Public.exe -screen-fullscreen 0 -popupwindow -screen-width 2560 -screen-height 1080 -monitor 0 -screen-quality Fantastic -window-mode exclusive -single-instance 
+
+
